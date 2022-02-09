c8544de057fc4f267a79f2586e257296b3bf42ef
Esmilder Lopez <esmilder10.lp@gmail.com>
2022-02-09 16:20:14 -0600

    Guardar

--- ControlesPersonalizados.UI/Prueba/FrmPrincipal.Designer.cs
+++ ControlesPersonalizados.UI/Prueba/FrmPrincipal.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace ControlesPersonalizados.UI
+﻿namespace ControlesPersonalizados.UI.Prueba
 {
-    partial class Form1
+    partial class FrmPrincipal
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,20 +23,20 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(546, 230);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(481, 388);
+            this.Name = "FrmPrincipal";
+            this.Text = "FrmPrincipal";
             this.ResumeLayout(false);
 
         }
